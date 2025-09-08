28be34c6c89b8ef1a823a1a6476f672cce650cc4
Md. Nafiz Ahmed <nafizahmed000@gmail.com>
2025-09-08 18:46:33 +0600

    first commit

---
+++ serve.bat
@@ -0,0 +1,8 @@
+@echo off
+REM Laravel Development Server Launcher
+REM This batch file ensures PHP is available and starts the Laravel server
+
+echo Starting Laravel Development Server...
+C:\xampp\php\php.exe artisan serve
+
+pause
